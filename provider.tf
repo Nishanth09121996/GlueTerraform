2ec8ce18a9729acf8efd68e70bbaef8340a264ec
Nishanth09121996 <nishanthkumar021@gmail>
2021-12-14 17:19:15 +0530

    changed files

--- provider.tf
+++ provider.tf
@@ -1,15 +1,6 @@
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
   region     = var.aws_region
 
-}
-terraform {
-    backend "s3" {
-      encrypt = true
-      bucket = "terraform-glue-bucket"
-      
-      key = "terraformbackend/terraform.tfstate"
-      region = "us-east-1"
-  }
 }
